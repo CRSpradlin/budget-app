aea067b8619e0103cfe92ba5e12c20e5933d7310
crspradlin <github@crspradlin.org>
2024-04-18 22:55:40 -0400

    Updated styling for a more clean, centered look

--- src/client/root.tsx
+++ src/client/root.tsx
@@ -42,18 +42,19 @@ export class Root extends React.Component {
 
 	public render() {
 		return (
-			<div className="h-full">
-				<div className="flex flex-row items-center p-10">
-					<form id="form" onSubmit={this.handleSubmit}>
-						<div className="m-5">
-							<label>New A1 Cell Value: </label>
-							<input name="newValue" value={this.state.newA1CellValue} onChange={(e) => this.setState({ newA1CellValue: e.target.value })} type="text" required/>
-						</div>
-						<div className="m-10">
-							<input id="submit" type="submit" value={this.state.loading?"Submitting...":"Submit"} disabled={this.state.loading} className={`w-[10rem] ${this.state.loading ? 'bg-indigo-500' : ' bg-indigo-800 hover:bg-indigo-500'} px-5 py-2 text-sm rounded-full font-semibold text-white`}/>
-						</div>
-					</form>
-				</div>
+			<div className="h-full flex flex-row content-center">
+				<form className="flex flex-col m-auto pb-40 items-center" id="form" onSubmit={this.handleSubmit}>
+					<div>
+						<h1 className="text-xl">Modify Cell A1</h1>
+					</div>
+					<div className="mt-10">
+						<label>New Cell Value: </label>
+						<input name="newValue" value={this.state.newA1CellValue} onChange={(e) => this.setState({ newA1CellValue: e.target.value })} type="text" required/>
+					</div>
+					<div className="mt-5">
+						<input id="submit" type="submit" value={this.state.loading?"Submitting...":"Submit"} disabled={this.state.loading} className={`w-[10rem] ${this.state.loading ? 'bg-indigo-500' : ' bg-indigo-800 hover:bg-indigo-500'} px-5 py-2 text-sm rounded-full font-semibold text-white`}/>
+					</div>
+				</form>
 			</div>
 		);
 	}
